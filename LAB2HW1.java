bf051a99f19797a9a047394b3f79923980724929
Farhan Tanvir <tanvirfarhan2002@gmail.com>
2026-03-18 22:01:28 +0600

    Update and rename L2HW1.java to LAB2HW1.java
    Added question and renamed file.

--- LAB2HW1.java
+++ LAB2HW1.java
@@ -1,6 +1,9 @@
+// Write a Java code of a program that takes an integer number as user input and then determines if that number is divisible by either 5 or 7 but not both; 
+// otherwise display “No”. For example, numbers like 35, 70, 105, 140, 175, 210, 245, 280 etc. can be divisible by both 5 and 7. These numbers are invalid.
+
 import java.util.Scanner;
 
-public class L2HW1 {
+public class LAB2HW1 {
   public static void main (String [] args) {
     Scanner sc = new Scanner(System.in);
     int num = sc.nextInt();
