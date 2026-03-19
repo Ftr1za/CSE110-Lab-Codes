1be8f6e49f9cf00f2b893a8a7677c4e0747f8c7f
Farhan Tanvir <tanvirfarhan2002@gmail.com>
2026-03-20 03:56:41 +0600

    Update and rename L3HW1.java to LAB3HW1.java

--- LAB3HW1.java
+++ LAB3HW1.java
@@ -1,6 +1,8 @@
+//Write a Java program that will take a positive integer n as input and print all the numbers from 0 to n which are divisible by 5 but not divisible by 3.
+
 import java.util.Scanner;
 
-public class L3HW1{
+public class LAB3HW1{
   public static void main(String [] args){
     Scanner sc = new Scanner(System.in);
     int num = sc.nextInt();
